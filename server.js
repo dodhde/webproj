bd65ef26e06e062653121695b93f9f43e9202ad4
dodhde <144246247+dodhde@users.noreply.github.com>
2024-06-07 02:01:16 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const ejs = require('ejs');
 const app = express();
 const port = 8002;
 const bodyParser = require('body-parser');
-const mariadb = require('mariadb/callback');
+const mariadb = require('mariadb');
 
 require('dotenv').config();
 
@@ -24,58 +24,40 @@ app.use(express.json());
 
 
 
-app.get('/', (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error: ' + err.stack);
-      return res.status(500).send('Error');
-    }
-
-    const sql = 'SELECT * FROM writesub';
-    connection.query(sql, (err, results) => {
-      connection.release();
-      if (err) {
-        console.error('Error: ' + err.stack);
-        return res.status(500).send('Error');
-      }
-      res.render('index', { posts: results });
-    });
-  });
+app.get('/', async (req, res) => {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query('SELECT * FROM writesub');
+    res.render('index', { posts: rows });
+  } catch (err) {
+    console.error('Error: ' + err.stack);
+    res.status(500).send('Error');
+  } finally {
+    if (conn) conn.release();
+  }
 });
 
 app.get('/write', function(req, res){
     res.render('write.ejs')    
-})
-app.get('/login', function(req, res){
-    res.render('login.ejs')    
-})
+}
 
+app.post('/writesub', async (req, res) => {
+  const { name, password, ingredients, title, content } = req.body;
 
-app.post('/writesub', (req, res) => {
-    const name = req.body.name;
-    const password = req.body.password;
-    const ingredients = req.body.ingredients;
-    const title = req.body.title;
-    const content = req.body.content; 
-
-    const sql = `INSERT INTO writesub (name, password, ingredients, title, content, regdate) VALUES (?, ?, ?, ?, ?, NOW())`;
-  
-    pool.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error: ' + err.stack);
-      return res.status(500).send('Error');
-    }
-
-    connection.query(sql, [name, password, ingredients, title, content], (err, result) => {
-      connection.release();
-      if (err) {
-        console.error('Error: ' + err.stack);
-        return res.status(500).send('Error');
-      }
-      console.log('글 등록', new Date().toLocaleString());
-      res.send("<script>alert('글이 등록되었습니다'); location.href='/'</script>");
-    });
-  });
+  const sql = `INSERT INTO writesub (name, password, ingredients, title, content, regdate) VALUES (?, ?, ?, ?, ?, NOW())`;
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    await conn.query(sql, [name, password, ingredients, title, content]);
+    console.log('글 등록', new Date().toLocaleString());
+    res.send("<script>alert('글이 등록되었습니다'); location.href='/'</script>");
+  } catch (err) {
+    console.error('Error: ' + err.stack);
+    res.status(500).send('Error');
+  } finally {
+    if (conn) conn.release();
+  }
 });
 
 app.listen(port, '0.0.0.0', () => {
