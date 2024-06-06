079567796e36c60628e80e9dad6eed0adcf91647
dodhde <144246247+dodhde@users.noreply.github.com>
2024-06-07 00:27:02 +0900

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ const connection = mariadb.createConnection({
   user     : process.env.DB_USERNAME,
   password : process.env.DB_PASSWORD,
   database : process.env.DB_NAME,
+  connectionLimit: 10
+})
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('DB connection failed: ' + err.stack);
+    return;
+  }
+  console.log('Connected to DB.');
+  connection.release();
 })
 
 
