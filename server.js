1f0a0ab54087f22be445c14622da055193babb57
dodhde <144246247+dodhde@users.noreply.github.com>
2024-06-07 13:43:56 +0900

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 const pool = mariadb.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  user: process.env.DB_USER,
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
